8b83b28fac619d72e7aea29c4cf7c654f2866224
MOHOAzure <timecrutramer@gmail.com>
2020-11-30 21:17:25 +0800

    correct cloud tz issue #8

--- Scheduler.py
+++ Scheduler.py
@@ -1,20 +1,47 @@
-import schedule, time
+import schedule, time, datetime
 
 import logging, logging.config
 logging.config.fileConfig('logging.conf')
 console_logger=logging.getLogger()
 
 from MainReporter import report_current_weather, report_forecast_weather, report_fb_page_news, report_pic
-import config
+import config, config_schedule
 
+def convert_time(tz, the_time):
+    """
+    type:
+        tz: the specified time zone of schedule
+        the_time: the scheduled time (HH:MM)
+    
+    rtype:
+        the_time: the scheduled time (HH:MM) based on the specified tz regardless of server's time zone
+    """
+    machine_tz_sec= (time.localtime().tm_gmtoff) # get the time zone of machine, tm_gmtoff returns east of UTC in seconds
+    delta_zone = tz[0]
+    delta_hour = int(tz[1:3])
+    delta_minute = int(tz[3:])
+    delta_sec = (delta_hour*3600)+(delta_minute*60)
+    the_time = datetime.datetime.strptime(the_time, "%H:%M")
+    delta = datetime.timedelta(seconds= delta_sec-machine_tz_sec)
+
+    if delta_zone == "+":
+        the_time -= delta
+    elif delta_zone == "-":
+        the_time += delta
+        
+    return the_time.strftime("%H:%M")
+    
 def run():
     # set up time to report weather
-    schedule.every().day.at( config.report_forecast_weather_time ).do( report_forecast_weather )
-    for eachtime in config.report_current_weather_time:
+    report_forecast_weather_time = convert_time(config_schedule.tz, config_schedule.report_forecast_weather_time)
+    schedule.every().day.at( report_forecast_weather_time ).do( report_forecast_weather )
+    for eachtime in config_schedule.report_current_weather_time:
+        eachtime=convert_time(config_schedule.tz, eachtime)
         schedule.every().day.at(eachtime).do( report_current_weather )
 
     # set up time to report news
-    for eachtime in config.report_fb_page_news_time:
+    for eachtime in config_schedule.report_fb_page_news_time:
+        eachtime=convert_time(config_schedule.tz, eachtime)
         schedule.every().day.at(eachtime).do( report_fb_page_news )
 
     # set up time to report pictures
@@ -25,3 +52,4 @@ def run():
     while True:
         schedule.run_pending()
         time.sleep(1)
+        
---
+++ config_schedule.py
@@ -0,0 +1,11 @@
+# UTC time zone of the following schedule (+HH:MM)
+tz="+0800"
+
+# time to report current weather (HH:MM)
+report_current_weather_time=['07:00','12:00','17:00','22:00']
+
+# time to report tomorrow weather forecast (HH:MM)
+report_forecast_weather_time='22:00'
+
+# time to report fb page (HH:MM)
+report_fb_page_news_time=['07:00','12:00','17:00','22:00']

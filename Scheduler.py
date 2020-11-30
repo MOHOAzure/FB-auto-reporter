e3a05f81f72660bd84bfa5f11bb1a6764be49457
MOHOAzure <timecrutramer@gmail.com>
2020-11-30 12:27:15 +0800

    report pic hoursly

--- Scheduler.py
+++ Scheduler.py
@@ -18,7 +18,7 @@ def run():
         schedule.every().day.at(eachtime).do( report_fb_page_news )
 
     # set up time to report pictures
-    schedule.every().hours.do( report_pic )
+    schedule.every().hour.do( report_pic )
 
     # run scheduler
     console_logger.info("Tasks are scheduled")
